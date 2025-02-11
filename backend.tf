791826a2172d4db9bc5285807c2e49e232a67010
ayon2016 <ayon.choudhury2016@gmail.com>
2025-02-11 17:21:39 -0500

    changed bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tsc-terraform-artifacts-test"
+    bucket         = "tsc-terraform-artifact-test"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
