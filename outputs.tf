3107833fa0e6027842dd626efd35457a10c98dbc
ayon2016 <ayon.choudhury2016@gmail.com>
2025-02-11 12:11:03 -0500

    Added outputs for public ip

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "alb_dns_name" {
   value = aws_lb.tsc_poc_alb.dns_name
 }
 
-output "master_public_ip" {
+output "locust_master_public_ip" {
   value = aws_instance.tsc_poc_locust_master.public_ip
 }
 
-output "worker_public_ip" {
+output "locust_worker_public_ip" {
   value = aws_instance.tsc_poc_locust_worker.public_ip
 }
 
