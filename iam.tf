44bd998144306c5d944c5ce80a1e1eedb1c410a7
ayon2016 <ayon.choudhury2016@gmail.com>
2025-02-12 18:39:30 -0500

    S3 list bucket

--- iam.tf
+++ iam.tf
@@ -32,8 +32,10 @@ resource "aws_iam_policy" "ec2_s3_policy" {
           "s3:ListBucket"
         ]
         Resource = [
-          "arn:aws:s3:::your-locust-reports-bucket",
-          "arn:aws:s3:::your-locust-reports-bucket/*"
+          "arn:aws:s3:::tsc-terraform-artifact-test",
+          "arn:aws:s3:::tsc-terraform-artifact-test/*",
+          "arn:aws:s3:::tsc-locust-reports",
+          "arn:aws:s3:::tsc-locust-reports/*"
         ]
       }
     ]
