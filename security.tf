039afc584b771b4dec35fa1c28d59efa94fb98dd
ayon2016 <ayon.choudhury2016@gmail.com>
2025-02-13 15:01:23 -0500

    Added connectivity over 5557

--- security.tf
+++ security.tf
@@ -41,6 +41,13 @@ resource "aws_security_group" "tsc_poc_ec2_locust_sg" {
     cidr_blocks = ["10.0.0.0/16"]
   }
 
+   ingress {
+    from_port   = 5557
+    to_port     = 5557
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
   # Allow outbound traffic
   egress {
     from_port   = 0
