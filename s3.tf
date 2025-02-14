d4061e8961db758443eae0fd9153597d820ef01a
ayon2016 <ayon.choudhury2016@gmail.com>
2025-02-14 16:00:25 -0500

    Deleting the S3 setup

--- s3.tf
+++ s3.tf
@@ -34,38 +34,38 @@
 #   restrict_public_buckets = true
 # }
 
-resource "aws_s3_bucket" "locust_reports" {
-  bucket = "tsc-locust-reports"
+# resource "aws_s3_bucket" "locust_reports" {
+#   bucket = "tsc-locust-reports"
 
-  tags = {
-    Name        = "LocustReports"
-    Environment = "Production"
-  }
-}
+#   tags = {
+#     Name        = "LocustReports"
+#     Environment = "Production"
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "locust_reports_versioning" {
-  bucket = aws_s3_bucket.locust_reports.id
+# resource "aws_s3_bucket_versioning" "locust_reports_versioning" {
+#   bucket = aws_s3_bucket.locust_reports.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "locust_reports_encryption" {
-  bucket = aws_s3_bucket.locust_reports.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "locust_reports_encryption" {
+#   bucket = aws_s3_bucket.locust_reports.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "locust_reports_block" {
-  bucket = aws_s3_bucket.locust_reports.id
+# resource "aws_s3_bucket_public_access_block" "locust_reports_block" {
+#   bucket = aws_s3_bucket.locust_reports.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
